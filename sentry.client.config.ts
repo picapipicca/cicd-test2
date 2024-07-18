9ccf4454bcd1376a6c570e70880200a0ccf0b1b9
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2024-07-18 16:28:11 +0900

    reset config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  // ignoreErrors: ["Something went wrong"],
+  ignoreErrors: ["Something went wrong"],
   dsn: "https://7f7d20cc0644490d14e1453735e49171@o4507620252516352.ingest.de.sentry.io/4507620318183504",
 
   // Adjust this value in production, or use tracesSampler for greater control
