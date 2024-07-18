7db6f47fb4b769c992f6127d56278af2376a8636
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2024-07-18 15:47:53 +0900

    fix

--- app/api/sentry-example-api/route.ts
+++ app/api/sentry-example-api/route.ts
@@ -3,11 +3,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 export const dynamic = "force-dynamic";
 
 // A faulty API route to test Sentry's error monitoring
-// export function GET() {
-//   throw new Error("Sentry Example API Route Error");
-//   return NextResponse.json({ data: "Testing Sentry Error..." });
-// }
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export function GET(req: NextApiRequest, res: NextApiResponse) {
   throw new Error("API throw error test");
-};
+}
