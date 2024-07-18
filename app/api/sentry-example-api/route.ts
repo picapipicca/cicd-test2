ee07210a9c8eb5eded93eee5957c3802492d849d
HAEWON LEE <90237570+picapipicca@users.noreply.github.com>
2024-07-18 10:59:36 +0900

    sentry test

---
+++ app/api/sentry-example-api/route.ts
@@ -0,0 +1,15 @@
+// import { NextResponse } from "next/server";
+
+// export const dynamic = "force-dynamic";
+
+// // A faulty API route to test Sentry's error monitoring
+// export function GET() {
+//   throw new Error("Sentry Example API Route Error");
+//   return NextResponse.json({ data: "Testing Sentry Error..." });
+// }
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  throw new Error("API throw error test");
+  res.status(200).json({ name: "John Doe" });
+};
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7f7d20cc0644490d14e1453735e49171@o4507620252516352.ingest.de.sentry.io/4507620318183504",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
